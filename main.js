b399ce409edcfcf4d82f3dd53f25c60c3f3345bf
Asafe Gamaliel <asafegamaliel0@gmail.com>
2022-11-02 19:52:39 -0300

    (js): criado a função criarCasaDoTabuleiro com parametro coordenadas, cria uma casa do tabuleiro com a classe de mesmo nome e retorna a tag html

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ function criarTabuleiro() {
 
     for (let row = 0; row < 8; row++) {
         for (let column = 0; column < 8; column++) {
-            const casaDoTabuleiro = document.createElement("div");
+            const casaDoTabuleiro = criarCasaDoTabuleiro({row, column});
 
             tabuleiro.appendChild(casaDoTabuleiro);
         }
     }
 }
+
+function criarCasaDoTabuleiro(coordenadas) {
+    const casaDoTabuleiro = document.createElement("div");
+    casaDoTabuleiro.classList.add("casa-do-tabuleiro");
+
+    return casaDoTabuleiro;
+} 
 
 criarTabuleiro();
